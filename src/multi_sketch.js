0445c58ef4551c077841e5db58c2256beb537abe
Andrea <andrea@kaist.ac.kr>
2021-12-16 12:44:20 +0900

    Init

---
+++ src/multi_sketch.js
@@ -0,0 +1,40 @@
+import '../css/style.css';
+import sketch from 'p5js-wrapper';
+
+let sketch1 = new p5( p => {
+
+    p.setup = () => {
+      // canvas size is specified in the CSS file (size of div #one)
+      const one= document.getElementById('one');
+      p.createCanvas(one.clientWidth, one.clientHeight);
+      // same as: p.createCanvas($("#one").width(), $("#one").height());
+    };
+  
+    p.draw = () => {
+      p.background(100);
+      p.fill(255);
+      p.noStroke();
+      p.rectMode(p.CENTER);
+      p.rect(p.mouseX, p.mouseY, 50, 50);
+  
+    };
+  }, 'one');
+  
+  
+  // Sketch2
+  let sketch2 = new p5( p => {
+  
+    p.setup = () => {
+      // canvas size is specified in the CSS file (size of div #two)
+      const two= document.getElementById('two');
+      p.createCanvas(two.clientWidth, two.clientHeight);
+      // same as: p.createCanvas($("#two").width(), $("#two").height());
+    };
+  
+    p.draw = () => {
+      p.background(170);
+      p.noStroke();
+      p.fill(255);
+      p.ellipse(p.mouseX, p.mouseY, 50, 50);
+    };
+  }, 'two');
---
+++ src/single_sketch.js
@@ -0,0 +1,18 @@
+import '../css/style.css';
+import sketch from 'p5js-wrapper';
+
+sketch.setup = function(){
+  createCanvas (800, 600);
+}
+
+sketch.draw= function(){
+  background(100);
+  fill(255, 0, 0);
+  noStroke();
+  rectMode(CENTER);
+  rect(mouseX, mouseY, 50, 50);
+}
+
+sketch.mousePressed = function(){
+  console.log('here');
+}
